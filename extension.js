3f5507fc095bdb791c6347e6d9e695100350ac44
dawa <962473195@qq.com>
2022-03-18 17:44:02 +0800

    push command

--- extension.js
+++ extension.js
@@ -1,7 +1,9 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-const vscode = require('vscode');
-
+const vscode = require("vscode");
+const child_process = require("child_process");
+const { argv } = require("process");
+const path = require("path");
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 
@@ -9,28 +11,45 @@ const vscode = require('vscode');
  * @param {vscode.ExtensionContext} context
  */
 function activate(context) {
+  // Use the console to output diagnostic information (console.log) and errors (console.error)
+  // This line of code will only be executed once when your extension is activated
+  console.log('Congratulations, your extension "my-extension" is now active!');
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "my-extension" is now active!');
+  // The command has been defined in the package.json file
+  // Now provide the implementation of the command with  registerCommand
+  // The commandId parameter must match the command field in package.json
+  let disposableHelloWord = vscode.commands.registerCommand(
+    "my-extension.helloWorld",
+    function () {
+      // The code you place here will be executed every time your command is executed
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with  registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('my-extension.helloWorld', function () {
-		// The code you place here will be executed every time your command is executed
+      // Display a message box to the user
+      vscode.window.showInformationMessage("Hello World from my-extension!");
+    }
+  );
+  let disposablePushGerrit = vscode.commands.registerCommand(
+    "my-extension.pushGerrit",
+    function () {
+      let branch = "master";
+      child_process.exec("git status", (error, stdout, stderr) => {
+        branch = stdout;
+        console.log(branch, error, stderr, stdout);
+        vscode.window.showInformationMessage(`branch:${branch.toString()}`);
+      });
+      debugger;
+      console.log(vscode.workspace.workspaceFolders);
+      vscode.window.showInformationMessage(`branch:${branch}`);
+      vscode.commands.executeCommand("git.push");
+    }
+  );
 
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from my-extension!');
-	});
-
-	context.subscriptions.push(disposable);
+  context.subscriptions.push(disposablePushGerrit, disposableHelloWord);
 }
 
 // this method is called when your extension is deactivated
 function deactivate() {}
 
 module.exports = {
-	activate,
-	deactivate
-}
+  activate,
+  deactivate,
+};
